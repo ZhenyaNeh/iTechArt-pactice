ad433e530fc051ca61a46b895c32c2946c1b3668
ZhenyaNeh <zhenya.nehaychik@gmail.com>
2025-06-26 11:28:29 +0300

    feat: add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { Counter } from './components/Counter';
-import "./index.scss"
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import "./index.scss";
+import { Suspense } from "react";
 
 const App = () => {
   return (
-    <div className='app'>
-      <h1>Counter Page</h1>
-      <Counter />  
+    <div className="app">
+      <Link to={"/"}>MainPage</Link>
+      <Link to={"/about"}>AboutPage</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/"} element={<MainPageAsync />} />
+          <Route path={"/about"} element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
