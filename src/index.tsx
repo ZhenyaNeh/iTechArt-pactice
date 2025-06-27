c41a9644d00dd063d6ee5bd52dcbd62db9ad0b07
ZhenyaNeh <zhenya.nehaychik@gmail.com>
2025-06-27 12:51:59 +0300

    feat: add theme providor

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import ThemeProvidor from "./theme/ThemeProvidor";
 
 const domNode = document.getElementById("root");
 
@@ -8,7 +9,9 @@ if (domNode) {
   const root = createRoot(domNode);
   root.render(
     <BrowserRouter>
-      <App />
+      <ThemeProvidor>
+        <App />
+      </ThemeProvidor>
     </BrowserRouter>
   );
 } else {
