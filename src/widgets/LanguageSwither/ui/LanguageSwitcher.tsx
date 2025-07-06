bf975498b850ed9a5ec8c2e3a0838dbeceb99a89
ZhenyaNeh <zhenya.nehaychik@gmail.com>
2025-07-06 14:20:00 +0300

    feat: add language translation

---
+++ src/widgets/LanguageSwither/ui/LanguageSwitcher.tsx
@@ -0,0 +1,22 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./LanguageSwitcher.module.scss";
+import { useTranslation } from "react-i18next";
+import { Button } from "shared/ui/Button/Button";
+
+interface LanguageSwitcherProps {
+  className?: string;
+}
+
+export const LanguageSwither = ({ className = "" }: LanguageSwitcherProps) => {
+  const { t, i18n } = useTranslation();
+
+  const toggleLanguage = () => {
+    i18n.changeLanguage(i18n.language === "ru" ? "en" : "ru");
+  };
+
+  return (
+    <Button onClick={toggleLanguage} className={classNames([cls["language-switcher"], className])}>
+      {t("Язык")}
+    </Button>
+  );
+};
