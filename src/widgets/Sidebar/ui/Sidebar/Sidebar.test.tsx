60ed2380c98bcf80c03a82c8bf994ef81ad9ac73
ZhenyaNeh <zhenya.nehaychik@gmail.com>
2025-07-10 11:09:25 +0300

    feat: add ui tests

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import { Sidebar } from './Sidebar';
+
+describe('Sidebar test', () => {
+  test('Sidebar render', () => {
+    render(<Sidebar />);
+    expect(screen.getByTestId('sidebar')).toBeInTheDocument();
+  });
+  
+});
